a2c5591c7adb834cfd11d45ffe73cbada9946416
quan_yuan <quan.yuan@yonolabs.com>
2016-01-31 23:50:27 -0800

    add cross validation

--- cvMachine.py
+++ cvMachine.py
@@ -1,7 +1,7 @@
 
-def cvScore(clf, X, Y):
+def cvScore(clf, X, Y, cv=5):
 	from sklearn import cross_validation
-	return cross_validation.cross_val_score(clf, X, Y, cv=5)
+	return cross_validation.cross_val_score(clf, X, Y, cv=cv)
 
 def paramSelector(clf, params, X, Y):
 	from sklearn.grid_search import GridSearchCV
@@ -9,3 +9,16 @@ def paramSelector(clf, params, X, Y):
 	clf = GridSearchCV(clf, params)
 	clf.fit(X, Y)
 	return clf.best_estimator_, clf.grid_scores_
+
+def cross_validation(clf, X, Y, cv=5):
+	df['row'] = range(len(df))
+	df.set_index(['row'],inplace=True)
+	k = [(len(df)-1)/cv*j for j in range(cv+1)]
+	score = [0.0]*cv
+	for i in range(cv):		
+		train = pd.concat([df.loc[:k[i],:],df.loc[k[i+1]:,:]])
+		test = df.loc[k[i]:k[i+1],:]
+		model = lm.fit(train,x_name,y_name,par)
+		pred = lm.predict(test[x_name],model)
+		score[i] = (pred == test[y_name]).sum()/float(len(test))
+	return sum(score)/float(cv)
