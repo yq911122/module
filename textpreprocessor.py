fe33bacaa4a0e22b7210a3aa5c93462589c213d6
quan_yuan <quan.yuan@yonolabs.com>
2016-02-12 10:27:14 -0800

    improve stop words

--- textpreprocessor.py
+++ textpreprocessor.py
@@ -22,6 +22,12 @@ def get_corpus(s):
 	s.map(lambda x: l.extend(x))
 	return l
 
+def get_stop_words_and_freq(s, n):
+	from collections import Counter
+	l = get_corpus(s)
+	l = [x for x in Counter(l).most_common(n)]
+	return l
+
 def get_stop_words(s, n):
 	'''
 	:s : pd.Series; each element as a list of words from tokenization
@@ -34,7 +40,7 @@ def get_stop_words(s, n):
 	l = [x[0] for x in Counter(l).most_common(n)]
 	return l
 
-def remove_stop_words(s, n, nltk = False):
+def remove_stop_words(s, n, stop_words = None, nltk = False):
 	'''
 	:s : pd.Series; each element as a list of words from tokenization
 	:n : int; n most frequent words are judged as stop words 
@@ -44,7 +50,7 @@ def remove_stop_words(s, n, nltk = False):
 	if nltk: 
 		from nltk.corpus import stopwords
 		stop_words = stopwords.words('english')
-	else: stop_words = get_stop_words(s, n)
+	elif not stop_words: stop_words = get_stop_words(s, n)
 	return s.map(lambda x: [e for e in x if e not in stop_words])
 
 def to_tfidf(s, cv=None, tfidf=None, stop_words=None):
