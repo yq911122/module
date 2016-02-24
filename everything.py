0df1a5b8e76e036729b2f4549480db8dcdb05ef9
quan_yuan <quan.yuan@yonolabs.com>
2016-02-23 23:24:56 -0800

    add calculate prime factors

--- everything.py
+++ everything.py
@@ -21,8 +21,20 @@ def dictFromList(keys, values=None):
 	return dict(zip(keys,values))
 
 def static_vars(**kwargs):
-    def decorate(func):
-        for k in kwargs:
-            setattr(func, k, kwargs[k])
-        return func
-    return decorate
+	def decorate(func):
+		for k in kwargs:
+			setattr(func, k, kwargs[k])
+		return func
+	return decorate
+
+def primes(n):
+	primfac = []
+	d = 2
+	while d*d <= n:
+		while (n % d) == 0:
+			primfac.append(d)  # supposing you want multiple factors repeated
+			n //= d
+		d += 1
+	if n > 1:
+	   primfac.append(n)
+	return primfac
